f90ac97a7f2b614d38fe665a162ac8db2439e442
masao <masao@keme.jp>
2017-05-18 17:58:48 +0900

    chapter05 Q8

--- src/chapter05/DateAndTime08.java
+++ src/chapter05/DateAndTime08.java
@@ -3,31 +3,24 @@
  * 現在の時刻インスタントに対してサポートされる全てのタイムゾーンにおいて、
  * 今日の日付のオフセット（UTCとの差）を取得しなさい。
  * その際、ZoneId.getAvailableIdsをストリームに変換してから、ストリーム操作を使用することによって取得しなさい。
- *
+ * <p>
  * 【メモ】
  * ひとまず出力させるぞっていうのはできたが、他の方法もあるのではないか、そもそも違うんじゃないか、と思い見直し中。
+ * -> ジョンさんに相談して解決した!
  */
 package chapter05;
 
-import com.sun.scenario.effect.Offset;
-
 import java.time.*;
-import java.util.Set;
 import java.util.stream.Stream;
 
 public class DateAndTime08 {
-    public static void main(String[] args){
-        ZonedDateTime datetime = ZonedDateTime.now();
-        Instant instant = datetime.toInstant();
+    public static void main(String[] args) {
+        Instant instant = ZonedDateTime.now().toInstant();
 
-        Set zoneIdStream = ZoneId.getAvailableZoneIds();
-
-        Stream offsetStream = zoneIdStream.stream().peek(zone ->
-                System.out.print(
-                    instant.atZone( ZoneId.of(zone.toString()) ).getOffset()
-                )
+        Stream offsetStream = ZoneId.getAvailableZoneIds().stream().map(zone ->
+                instant.atZone(ZoneId.of(zone)).getOffset()
         );
 
-        offsetStream.forEach( x -> System.out.println( " " + x.toString()));
+        offsetStream.forEach(x -> System.out.println(x.toString()));
     }
 }
